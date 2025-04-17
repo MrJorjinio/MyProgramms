6c7e739097c1dd41dc51528089bf0543f30d646a
MrJorjinio <jorjiniomr@gmail.com>
2025-04-17 14:09:27 +0500

    2 - modul first hw

---
+++ WorkspaceProgram/ConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp;
+
+UserHome userHome = new UserHome();
+userHome.userHomeFillUp();
+userHome.userHomeShow();
---
+++ WorkspaceProgram/ConsoleApp/userHome.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class UserHome
+    { 
+        public string userHomeFirm { get; set; }
+        public string userHomeAdress { get; set; }
+
+        public double userHomeSize { get; set; }
+        public string userHomeType { get; set; }
+        public double userHomePrice { get; set; }
+        public int userHomeRooms { get; set; }
+        public string userName { get; set; }
+
+        public void userHomeFillUp()
+        {
+            Console.WriteLine("Enter your home firm: ");
+            userHomeFirm = Console.ReadLine();
+            Console.WriteLine("Enter your home adress: ");
+            userHomeAdress = Console.ReadLine();
+            Console.WriteLine("Enter your home size(m²): ");
+            userHomeSize = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your home type(Economy, Normal, Luxury): ");
+            userHomeType = Console.ReadLine();
+            Console.WriteLine("Enter your home price($): ");
+            userHomePrice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your home rooms: ");
+            userHomeRooms = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void userHomeShow()
+        {
+            Console.WriteLine("Your home firm: " + userHomeFirm);
+            Console.WriteLine("Your home adress: " + userHomeAdress);
+            Console.WriteLine("Your home size: " + userHomeSize + "m²");
+            Console.WriteLine("Your home type: " + userHomeType);
+            Console.WriteLine("Your home price: $" + userHomePrice);
+            Console.WriteLine("Your home rooms: " + userHomeRooms);
+        }
+
+    }
+}
